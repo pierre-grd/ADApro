d825d855a712391fe6706692add8c120c428c3b2
louisdelperugia <louis.delperugia@unil.ch>
2022-04-13 16:07:10 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,7 +4,9 @@ pd.set_option('display.max_columns', None)
 def load_data():
     training_df = pd.read_csv('log_mini.csv')
     label_train = pd.read_csv('tf_mini.csv')
-    return label_train, training_df
+    training_df.columns = training_df.columns.str.replace('track_id_clean', 'track_id')
+    df = pd.merge(training_df, label_train, on='track_id')
+    return df
 
-df_train, training_df = load_data()
-
+df = load_data()
+print(df.head())
