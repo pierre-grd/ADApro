49938069ccd13c1156a127a47056b2378e31859f
louisdelperugia <louis.delperugia@unil.ch>
2022-04-24 19:08:52 +0200

    hypertuning -> long process ...

--- main.py
+++ main.py
@@ -21,21 +21,21 @@ df = normalize_float(dummy_creation(load_data(training_path, label_path)))
 #====================
 
 
-acoust = ['acoustic_vector_0', 'acoustic_vector_1', 'acoustic_vector_2', 'acoustic_vector_3', 'acoustic_vector_4',
-          'acoustic_vector_5', 'acoustic_vector_6', 'acoustic_vector_7']
-matrix(df[acoust])
+#acoust = ['acoustic_vector_0', 'acoustic_vector_1', 'acoustic_vector_2', 'acoustic_vector_3', 'acoustic_vector_4',
+      #    'acoustic_vector_5', 'acoustic_vector_6', 'acoustic_vector_7']
+#matrix(df[acoust])
 
-hist_continuous(df)
+#hist_continuous(df)
 
-scatterplot_skip(df, "energy", "duration")
+#scatterplot_skip(df, "energy", "duration")
 
-categorical_col = ['session_position', 'session_length', 'context_switch',
-                   'no_pause_before_play', 'short_pause_before_play',
-                   'long_pause_before_play', 'hist_user_behavior_n_seekfwd', 'hist_user_behavior_n_seekback',
-                   'premium', 'context_type',
-                   'mode']
+#categorical_col = ['session_position', 'session_length', 'context_switch',
+ #                  'no_pause_before_play', 'short_pause_before_play',
+ #                  'long_pause_before_play', 'hist_user_behavior_n_seekfwd', 'hist_user_behavior_n_seekback',
+ #                  'premium', 'context_type',
+  #                 'mode']
 
-countplot(df, categorical_col)
+#countplot(df, categorical_col)
 
 #====================
 #Model
--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import classification_report
-
-
 
 
 def split_data(df):
@@ -12,16 +11,18 @@ def split_data(df):
                'valence', 'acoustic_vector_0', 'acoustic_vector_1', 'acoustic_vector_2', 'acoustic_vector_3',
                'acoustic_vector_4', 'acoustic_vector_5', 'acoustic_vector_6', 'acoustic_vector_7']]
     y_df = df["skipped"]
-    print(X_df)
-    print(y_df)
     X_train, X_test, y_train, y_test = train_test_split(X_df, y_df, test_size=0.4, random_state=0)
     return X_train, X_test, y_train, y_test
 
 
 def GBC_model(X_train, X_test, y_train, y_test):
     GBC_model = GradientBoostingClassifier()
+    parameters = {"n_estimators": [300, 500, 900],
+                  "learning_rate": [0.05, 0.5, 1]
+        ,
+                  "max_features": ['auto', 'sqrt'], "min_samples_leaf": [1, 9]}
+
+    GBC_model = RandomizedSearchCV(GBC_model, param_distributions=parameters, n_jobs=-1, n_iter=25, cv=6, verbose=5)
     GBC_model.fit(X_train, y_train)
     GBC_pred = GBC_model.predict(X_test)
-    print(GBC_pred)
-    print(y_test)
     return classification_report(y_test, GBC_pred)
